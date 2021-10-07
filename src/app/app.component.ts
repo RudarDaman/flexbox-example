9528510d7944e178d3c270f41369ab6e662fb1ec
RudarDaman <damanrudar@gmail.com>
2021-10-07 12:17:04 +0530

    Example 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,25 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  pokemons = [];
-  list = [];
 
   constructor() {}
 
   ngOnInit() {
-    this.list = [...Array(25).keys()];
-  }
-
-  addMon() {
-    if (this.list.length > 0) {
-      const randomNumber = Math.floor(Math.random() * this.list.length);
-      this.pokemons.push(this.list[randomNumber]);
-      this.list.splice(randomNumber, 1);
-    }
-  }
-
-  clear() {
-    this.pokemons = [];
-    this.list = [...Array(25).keys()];
   }
 }
